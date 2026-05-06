0f98fe4d3e4864c76739e21767767727da0883e0
Hanna Mårtensson <hm233dq@student.lnu.se>
2026-05-06 08:43:03 +0200

    Change port

--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,7 @@
 import { createServer } from 'http'
 import app from './app.js'
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 createServer(app).listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
