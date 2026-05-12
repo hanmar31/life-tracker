a15d72bb0cec337e34b57d10d10c8ab84ab1791b
Hanna Mårtensson <hm233dq@student.lnu.se>
2026-05-12 11:03:21 +0200

    Add controllers

---
+++ src/server/controllers/habitController.js
@@ -0,0 +1,68 @@
+import Habit from '../models/habit.js'
+
+/**
+ * Gets the habits.
+ *
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ */
+export const getHabits = async (req, res) => {
+  try {
+    const habits = await Habit.find()
+    res.json(habits)
+  } catch (error) {
+    res.status(500).json({ message: 'Could not fetch habits' })
+  }
+}
+
+/**
+ * Creates a new habit.
+ *
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ */
+export const createHabit = async (req, res) => {
+  try {
+    const { name, frequency } = req.body
+
+    const habit = new Habit({
+      name,
+      frequency
+    })
+
+    await habit.save()
+
+    res.status(201).json(habit)
+  } catch (error) {
+    res.status(500).json({
+      message: 'Could not create habit'
+    })
+  }
+}
+
+/**
+ * Deletes a habit.
+ *
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @returns {void}
+ */
+export const deleteHabit = async (req, res) => {
+  try {
+    const habit = await Habit.findByIdAndDelete(req.params.id)
+
+    if (!habit) {
+      return res.status(404).json({
+        message: 'Habit not found'
+      })
+    }
+
+    res.status(204).send()
+  } catch (error) {
+    console.error(error)
+
+    res.status(500).json({
+      message: 'Could not delete habit'
+    })
+  }
+}
