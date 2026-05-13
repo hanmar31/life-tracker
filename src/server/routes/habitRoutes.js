2590aba94b63567727c13bf2d275ad5ad819fb6a
Hanna Mårtensson <hm233dq@student.lnu.se>
2026-05-13 10:16:10 +0200

    Add editHabit to route

--- src/server/routes/habitRoutes.js
+++ src/server/routes/habitRoutes.js
@@ -2,7 +2,8 @@ import express from 'express'
 import {
   getHabits,
   createHabit,
-  deleteHabit
+  deleteHabit,
+  editHabit
 } from '../controllers/habitController.js'
 
 const router = express.Router()
@@ -10,6 +11,8 @@ const router = express.Router()
 router.get('/v1/habits', getHabits)
 
 router.post('/v1/habits', createHabit)
+
+router.put('/v1/habits/:id', editHabit)
 
 router.delete('/v1/habits/:id', deleteHabit)
 
